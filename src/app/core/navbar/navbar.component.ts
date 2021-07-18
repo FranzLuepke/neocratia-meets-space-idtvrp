7ed3a2dc8c3b2976663370bd97b757e54da89a0d
FranzLuepke <fk.luepke146@uniandes.edu.co>
2021-07-18 12:21:04 -0500

    Minor changes to navbar and contact us

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -9,18 +9,18 @@ export class NavbarComponent implements OnInit {
   @Output() togleMenu = new EventEmitter();
 
   navItems = [
-    {
-      name: 'WHY',
-      link: '#why'
-    },
-    {
-      name: 'HOW',
-      link: '#how'
-    },
-    {
-      name: 'WHAT',
-      link: '#what'
-    },
+    //{
+    //  name: 'WHY',
+    //  link: '#why'
+    //},
+    //{
+    //  name: 'HOW',
+    //  link: '#how'
+    //},
+    //{
+    //  name: 'WHAT',
+    //  link: '#what'
+    //},
     {
       name: 'Products',
       link: 'products'
